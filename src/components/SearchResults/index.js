565dad996036b3481f613d649abca37f566947a8
Brielle McCormick <briellemccormick@Brielles-MBP.fios-router.home>
2020-09-23 16:16:59 -0400

    working in search results index.js

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -3,13 +3,14 @@ import "./style.css";
 
 function SearchResults(props) {
   return (
-    <ul className="list-group search-results">
-      {props.employee.map(result => (
-        <li key={result} className="list-group-item">
-          <img alt="Employee" src={result} className="img-fluid" />
-        </li>
-      ))}
-    </ul>
+    // <ul className="list-group search-results">
+    //   {props.employee.map(result => (
+    //     <li key={result} className="list-group-item">
+    //       <img alt="Employee" src={result} className="img-fluid" />
+    //     </li>
+    //   ))}
+    // </ul>
+    console.log(props)
   );
 }
 
